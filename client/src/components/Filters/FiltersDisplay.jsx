e45023293c32b6d794c735bf3135ed974d705a26
Samuel STPL <samuelstpierrelariviere@gmail.com>
2020-12-09 14:26:57 -0500

    Changes

--- client/src/components/Filters/FiltersDisplay.jsx
+++ client/src/components/Filters/FiltersDisplay.jsx
@@ -39,7 +39,7 @@ const FiltersContainer = styled.div`
     display: flex;
     flex-direction: column;
     background-color: white;
-    height: 60px;
+    height: 40px;
     border-radius: 10px;
   }
 `;
@@ -52,14 +52,14 @@ const List = styled.ul`
 const Filter = styled.li`
   font-size: 1.5rem;
   @media (max-width: 500px) {
-    margin: 0px 20px;
+    margin: 0.5rem 20px;
   }
 `;
 
 const NoFilter = styled.p`
   @media (max-width: 500px) {
     align-self: center;
-    margin-top: 1.2rem;
+    margin-top: 0.8rem;
     color: gray;
     font-style: italic;
   }
